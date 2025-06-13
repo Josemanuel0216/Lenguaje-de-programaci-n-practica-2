2b603a93517bd4324606da58053a085930f5bbf5
Josemanuel0216 <j83896008@gmail.com>
2025-06-13 10:59:31 -0400

    Practica 2 formulario 1

--- Practica 2 formulario 1/Practica 2 formulario 1/Form1.Designer.cs
+++ Practica 2 formulario 1/Practica 2 formulario 1/Form1.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lblNumero = new Label();
+            btnDesarrollar = new Button();
+            txtNumero = new TextBox();
+            lstTabla = new ListBox();
+            SuspendLayout();
+            // 
+            // lblNumero
+            // 
+            lblNumero.AutoSize = true;
+            lblNumero.BackColor = Color.FromArgb(224, 224, 224);
+            lblNumero.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            lblNumero.Location = new Point(14, 15);
+            lblNumero.Name = "lblNumero";
+            lblNumero.Size = new Size(147, 19);
+            lblNumero.TabIndex = 0;
+            lblNumero.Text = "Agregue un numero:\r\n";
+            lblNumero.Click += label1_Click;
+            // 
+            // btnDesarrollar
+            // 
+            btnDesarrollar.BackColor = Color.Cyan;
+            btnDesarrollar.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            btnDesarrollar.Location = new Point(100, 68);
+            btnDesarrollar.Name = "btnDesarrollar";
+            btnDesarrollar.Size = new Size(150, 33);
+            btnDesarrollar.TabIndex = 1;
+            btnDesarrollar.Text = "Desarrollar Tabla";
+            btnDesarrollar.UseVisualStyleBackColor = false;
+            btnDesarrollar.Click += btnDesarrollar_Click;
+            // 
+            // txtNumero
+            // 
+            txtNumero.BackColor = Color.FromArgb(224, 224, 224);
+            txtNumero.Location = new Point(206, 15);
+            txtNumero.Name = "txtNumero";
+            txtNumero.Size = new Size(120, 23);
+            txtNumero.TabIndex = 2;
+            // 
+            // lstTabla
+            // 
+            lstTabla.BackColor = Color.FromArgb(224, 224, 224);
+            lstTabla.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lstTabla.FormattingEnabled = true;
+            lstTabla.ItemHeight = 15;
+            lstTabla.Location = new Point(100, 130);
+            lstTabla.Name = "lstTabla";
+            lstTabla.Size = new Size(150, 154);
+            lstTabla.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Silver;
+            ClientSize = new Size(343, 348);
+            Controls.Add(lstTabla);
+            Controls.Add(txtNumero);
+            Controls.Add(btnDesarrollar);
+            Controls.Add(lblNumero);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblNumero;
+        private Button btnDesarrollar;
+        private TextBox txtNumero;
+        private ListBox lstTabla;
     }
 }
--- Practica 2 formulario 1/Practica 2 formulario 1/Form1.cs
+++ Practica 2 formulario 1/Practica 2 formulario 1/Form1.cs
@@ -6,5 +6,28 @@ namespace Practica_2_formulario_1
         {
             InitializeComponent();
         }
-    }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDesarrollar_Click(object sender, EventArgs e)
+        {
+            lstTabla.Items.Clear();  // Borra lalista antes de agregar valores nuevos 
+        
+            if (int.TryParse(txtNumero.Text, out int numero))
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    int resultado = numero * i;
+                    lstTabla.Items.Add($"{numero} x {i} = {resultado}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese un número válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }  
 }
