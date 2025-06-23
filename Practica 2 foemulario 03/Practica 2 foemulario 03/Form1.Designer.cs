b0752b0253717f1e020270e6990b81a77969ff9c
Josemanuel0216 <j83896008@gmail.com>
2025-06-23 11:04:03 -0400

    Practica 2 formulario 03

---
+++ Practica 2 foemulario 03/Practica 2 foemulario 03/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Practica_2_foemulario_03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.cmbSalida = new System.Windows.Forms.ComboBox();
+            this.cmbEntrada = new System.Windows.Forms.ComboBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.BackColor = System.Drawing.Color.Cyan;
+            this.btnConvertir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvertir.Location = new System.Drawing.Point(69, 198);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(121, 23);
+            this.btnConvertir.TabIndex = 3;
+            this.btnConvertir.Text = "Convertir ";
+            this.btnConvertir.UseVisualStyleBackColor = false;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.BackColor = System.Drawing.Color.Cyan;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(66, 248);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(64, 13);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // cmbSalida
+            // 
+            this.cmbSalida.BackColor = System.Drawing.Color.Cyan;
+            this.cmbSalida.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbSalida.FormattingEnabled = true;
+            this.cmbSalida.Items.AddRange(new object[] {
+            "Centimetro",
+            "Pulgada",
+            "Metro"});
+            this.cmbSalida.Location = new System.Drawing.Point(69, 147);
+            this.cmbSalida.Name = "cmbSalida";
+            this.cmbSalida.Size = new System.Drawing.Size(121, 24);
+            this.cmbSalida.TabIndex = 5;
+            // 
+            // cmbEntrada
+            // 
+            this.cmbEntrada.BackColor = System.Drawing.Color.Cyan;
+            this.cmbEntrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbEntrada.FormattingEnabled = true;
+            this.cmbEntrada.Items.AddRange(new object[] {
+            "Metro",
+            "Centimetro",
+            "Pulgada"});
+            this.cmbEntrada.Location = new System.Drawing.Point(69, 91);
+            this.cmbEntrada.Name = "cmbEntrada";
+            this.cmbEntrada.Size = new System.Drawing.Size(121, 24);
+            this.cmbEntrada.TabIndex = 6;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.BackColor = System.Drawing.Color.Cyan;
+            this.txtCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCantidad.Location = new System.Drawing.Point(69, 40);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(121, 23);
+            this.txtCantidad.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(266, 293);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.cmbEntrada);
+            this.Controls.Add(this.cmbSalida);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnConvertir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.ComboBox cmbSalida;
+        private System.Windows.Forms.ComboBox cmbEntrada;
+        private System.Windows.Forms.TextBox txtCantidad;
+    }
+}
+
---
+++ Practica 2 foemulario 03/Practica 2 foemulario 03/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_2_foemulario_03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+
+        {
+            if (double.TryParse(txtCantidad.Text, out double cantidad))
+
+            {
+                double metros = ConvertirAMetros(cantidad, cmbEntrada.Text);
+
+
+                double resultado = ConvertirDesdeMetros(metros, cmbSalida.Text);
+
+
+                lblResultado.Text = $"Resultado: {resultado:F2} {cmbSalida.Text}";
+
+            }
+
+            else
+
+            {
+                MessageBox.Show("Introduce una cantidad válida.");
+            }
+        }
+        private double ConvertirAMetros(double valor, string unidad)
+        {
+            switch (unidad)
+            {
+                case "Metro":
+
+                    return valor;
+
+                case "Centímetro":
+                    return valor / 100;
+
+                case "Pulgada":
+
+                    return valor * 0.0254;
+
+                default:
+
+                    return valor;
+            }
+        }
+
+        private double ConvertirDesdeMetros(double metros, string unidad)
+        {
+            switch (unidad)
+            {
+                case "Metro":
+
+                    return metros;
+
+                case "Centímetro":
+
+
+                    return metros * 100;
+
+                case "Pulgada":
+
+
+                    return metros / 0.0254;
+
+                default:
+
+                    return metros;
+            }
+        }
+    }
+}
