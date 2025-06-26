4c2cf4fede4c1324c35d49fc927f4e0874536b6c
Josemanuel0216 <j83896008@gmail.com>
2025-06-26 10:23:56 -0400

    Practica 2 Formulario 04

---
+++ Practica 2 Formulario 04/Practica 2 Formulario 04/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Practica_2_Formulario_04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.txtPalabra = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.BackColor = System.Drawing.Color.Cyan;
+            this.btnVerificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerificar.Location = new System.Drawing.Point(105, 99);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(100, 24);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = false;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // txtPalabra
+            // 
+            this.txtPalabra.BackColor = System.Drawing.Color.Cyan;
+            this.txtPalabra.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPalabra.Location = new System.Drawing.Point(105, 44);
+            this.txtPalabra.Name = "txtPalabra";
+            this.txtPalabra.Size = new System.Drawing.Size(100, 21);
+            this.txtPalabra.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(321, 274);
+            this.Controls.Add(this.txtPalabra);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.TextBox txtPalabra;
+    }
+}
+
---
+++ Practica 2 Formulario 04/Practica 2 Formulario 04/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_2_Formulario_04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            string texto = txtPalabra.Text.Trim().ToLower();
+            string invertido = new string(texto.Reverse().ToArray());
+
+            if (texto == invertido && texto.Length > 0)
+            {
+                MessageBox.Show("Es un palindroma", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No es un palindroma", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
