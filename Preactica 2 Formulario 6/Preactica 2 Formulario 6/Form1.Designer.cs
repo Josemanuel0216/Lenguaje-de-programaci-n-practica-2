5f8a31c227275d90170cb47bfe7467f897f85633
Josemanuel0216 <j83896008@gmail.com>
2025-06-26 11:42:49 -0400

    Practica 2 Formulario 6

---
+++ Preactica 2 Formulario 6/Preactica 2 Formulario 6/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Preactica_2_Formulario_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.maskedTextBoxNumero = new System.Windows.Forms.MaskedTextBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // maskedTextBoxNumero
+            // 
+            this.maskedTextBoxNumero.BackColor = System.Drawing.Color.Cyan;
+            this.maskedTextBoxNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBoxNumero.Location = new System.Drawing.Point(67, 33);
+            this.maskedTextBoxNumero.Mask = "00000";
+            this.maskedTextBoxNumero.Name = "maskedTextBoxNumero";
+            this.maskedTextBoxNumero.Size = new System.Drawing.Size(100, 21);
+            this.maskedTextBoxNumero.TabIndex = 0;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.BackColor = System.Drawing.Color.Cyan;
+            this.btnVerificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerificar.Location = new System.Drawing.Point(67, 108);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(100, 27);
+            this.btnVerificar.TabIndex = 1;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = false;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(243, 239);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.maskedTextBoxNumero);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxNumero;
+        private System.Windows.Forms.Button btnVerificar;
+    }
+}
+
---
+++ Preactica 2 Formulario 6/Preactica 2 Formulario 6/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Preactica_2_Formulario_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            {
+                // Validar que el campo no esté vacío
+                if (string.IsNullOrWhiteSpace(maskedTextBoxNumero.Text))
+                {
+                    MessageBox.Show("Por favor, ingrese un número.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Convertir a entero
+                if (int.TryParse(maskedTextBoxNumero.Text, out int numero))
+                {
+                    if (numero <= 0)
+                    {
+                        MessageBox.Show("Ingrese un número entero positivo.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    if (EsNumeroPerfecto(numero))
+                    {
+                        MessageBox.Show($"{numero} es un número perfecto.", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show($"{numero} no es un número perfecto.", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Entrada no válida. Solo se permiten números.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private bool EsNumeroPerfecto(int numero)
+        {
+            int suma = 0;
+            for (int i = 1; i <= numero / 2; i++)
+            {
+                if (numero % i == 0)
+                {
+                    suma += i;
+                }
+            }
+            return suma == numero;
+        }
+    }
+}
+        
+    
+
