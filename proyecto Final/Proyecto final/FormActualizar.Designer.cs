83d94a42f39e326ebb2e054f32da336fa38e776a
Josemanuel0216 <j83896008@gmail.com>
2025-08-04 20:53:31 -0400

    Agregado archivo al repositorio

---
+++ proyecto Final/Proyecto final/FormActualizar.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace SistemaEmpleados
+{
+    partial class FormActualizar
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.txtBuscarID = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDepartamento = new System.Windows.Forms.TextBox();
+            this.txtCargo = new System.Windows.Forms.TextBox();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.dtpFechaInicio = new System.Windows.Forms.DateTimePicker();
+            this.cmbEstado = new System.Windows.Forms.ComboBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.labelBuscarID = new System.Windows.Forms.Label();
+            this.labelID = new System.Windows.Forms.Label();
+            this.labelNombre = new System.Windows.Forms.Label();
+            this.labelDepartamento = new System.Windows.Forms.Label();
+            this.labelCargo = new System.Windows.Forms.Label();
+            this.labelFechaInicio = new System.Windows.Forms.Label();
+            this.labelSalario = new System.Windows.Forms.Label();
+            this.labelEstado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBuscarID
+            // 
+            this.txtBuscarID.Location = new System.Drawing.Point(30, 40);
+            this.txtBuscarID.Name = "txtBuscarID";
+            this.txtBuscarID.Size = new System.Drawing.Size(120, 20);
+            this.txtBuscarID.TabIndex = 0;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(30, 90);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(200, 20);
+            this.txtID.TabIndex = 1;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(30, 140);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(200, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // txtDepartamento
+            // 
+            this.txtDepartamento.Location = new System.Drawing.Point(30, 190);
+            this.txtDepartamento.Name = "txtDepartamento";
+            this.txtDepartamento.Size = new System.Drawing.Size(200, 20);
+            this.txtDepartamento.TabIndex = 3;
+            // 
+            // txtCargo
+            // 
+            this.txtCargo.Location = new System.Drawing.Point(300, 140);
+            this.txtCargo.Name = "txtCargo";
+            this.txtCargo.Size = new System.Drawing.Size(200, 20);
+            this.txtCargo.TabIndex = 4;
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(30, 240);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(200, 20);
+            this.txtSalario.TabIndex = 5;
+            // 
+            // dtpFechaInicio
+            // 
+            this.dtpFechaInicio.Location = new System.Drawing.Point(300, 90);
+            this.dtpFechaInicio.Name = "dtpFechaInicio";
+            this.dtpFechaInicio.Size = new System.Drawing.Size(200, 20);
+            this.dtpFechaInicio.TabIndex = 6;
+            // 
+            // cmbEstado
+            // 
+            this.cmbEstado.Items.AddRange(new object[] {
+            "ACTIVO",
+            "INACTIVO"});
+            this.cmbEstado.Location = new System.Drawing.Point(300, 190);
+            this.cmbEstado.Name = "cmbEstado";
+            this.cmbEstado.Size = new System.Drawing.Size(200, 21);
+            this.cmbEstado.TabIndex = 7;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnBuscar.Location = new System.Drawing.Point(160, 38);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(90, 25);
+            this.btnBuscar.TabIndex = 8;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnActualizar.Location = new System.Drawing.Point(300, 280);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(90, 35);
+            this.btnActualizar.TabIndex = 9;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = false;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.btnCancelar.Location = new System.Drawing.Point(410, 280);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(90, 35);
+            this.btnCancelar.TabIndex = 10;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // labelBuscarID
+            // 
+            this.labelBuscarID.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBuscarID.Location = new System.Drawing.Point(30, 20);
+            this.labelBuscarID.Name = "labelBuscarID";
+            this.labelBuscarID.Size = new System.Drawing.Size(100, 23);
+            this.labelBuscarID.TabIndex = 11;
+            this.labelBuscarID.Text = "Ingrese ID a buscar";
+            // 
+            // labelID
+            // 
+            this.labelID.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelID.Location = new System.Drawing.Point(30, 70);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(100, 23);
+            this.labelID.TabIndex = 12;
+            this.labelID.Text = "ID";
+            // 
+            // labelNombre
+            // 
+            this.labelNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNombre.Location = new System.Drawing.Point(30, 120);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(100, 23);
+            this.labelNombre.TabIndex = 13;
+            this.labelNombre.Text = "Nombre";
+            // 
+            // labelDepartamento
+            // 
+            this.labelDepartamento.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDepartamento.Location = new System.Drawing.Point(30, 170);
+            this.labelDepartamento.Name = "labelDepartamento";
+            this.labelDepartamento.Size = new System.Drawing.Size(100, 23);
+            this.labelDepartamento.TabIndex = 14;
+            this.labelDepartamento.Text = "Departamento";
+            // 
+            // labelCargo
+            // 
+            this.labelCargo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCargo.Location = new System.Drawing.Point(300, 120);
+            this.labelCargo.Name = "labelCargo";
+            this.labelCargo.Size = new System.Drawing.Size(100, 23);
+            this.labelCargo.TabIndex = 15;
+            this.labelCargo.Text = "Cargo";
+            // 
+            // labelFechaInicio
+            // 
+            this.labelFechaInicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFechaInicio.Location = new System.Drawing.Point(300, 70);
+            this.labelFechaInicio.Name = "labelFechaInicio";
+            this.labelFechaInicio.Size = new System.Drawing.Size(100, 23);
+            this.labelFechaInicio.TabIndex = 16;
+            this.labelFechaInicio.Text = "Fecha de Inicio";
+            // 
+            // labelSalario
+            // 
+            this.labelSalario.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSalario.Location = new System.Drawing.Point(30, 220);
+            this.labelSalario.Name = "labelSalario";
+            this.labelSalario.Size = new System.Drawing.Size(100, 23);
+            this.labelSalario.TabIndex = 17;
+            this.labelSalario.Text = "Salario";
+            // 
+            // labelEstado
+            // 
+            this.labelEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEstado.Location = new System.Drawing.Point(300, 170);
+            this.labelEstado.Name = "labelEstado";
+            this.labelEstado.Size = new System.Drawing.Size(100, 23);
+            this.labelEstado.TabIndex = 18;
+            this.labelEstado.Text = "Estado";
+            // 
+            // FormActualizar
+            // 
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(550, 340);
+            this.Controls.Add(this.txtBuscarID);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtDepartamento);
+            this.Controls.Add(this.txtCargo);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.dtpFechaInicio);
+            this.Controls.Add(this.cmbEstado);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.labelBuscarID);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.labelNombre);
+            this.Controls.Add(this.labelDepartamento);
+            this.Controls.Add(this.labelCargo);
+            this.Controls.Add(this.labelFechaInicio);
+            this.Controls.Add(this.labelSalario);
+            this.Controls.Add(this.labelEstado);
+            this.Name = "FormActualizar";
+            this.Text = "Actualizar Empleado";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBuscarID;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDepartamento;
+        private System.Windows.Forms.TextBox txtCargo;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.DateTimePicker dtpFechaInicio;
+        private System.Windows.Forms.ComboBox cmbEstado;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label labelBuscarID;
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label labelNombre;
+        private System.Windows.Forms.Label labelDepartamento;
+        private System.Windows.Forms.Label labelCargo;
+        private System.Windows.Forms.Label labelFechaInicio;
+        private System.Windows.Forms.Label labelSalario;
+        private System.Windows.Forms.Label labelEstado;
+    }
+}
---
+++ proyecto Final/Proyecto final/FormActualizar.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaEmpleados
+{
+    public partial class FormActualizar : Form
+    {
+        private Empleado empleadoOriginal;
+
+        public FormActualizar()
+        {
+         
+            InitializeComponent();
+
+            txtID.Enabled = false;
+            HabilitarCampos(false);
+
+        }
+
+        private void NuevoInicio()
+        {
+            txtBuscarID.Text = "";
+            txtID.Text = "";
+            txtNombre.Text = "";
+            txtDepartamento.Text = "";
+            txtCargo.Text = "";
+            txtSalario.Text = "";
+            cmbEstado.SelectedItem = "";
+
+            HabilitarCampos(false);
+        }
+
+        private void HabilitarCampos(bool habilitar)
+        {
+            txtNombre.Enabled = habilitar;
+            txtDepartamento.Enabled = habilitar;
+            txtCargo.Enabled = habilitar;
+            dtpFechaInicio.Enabled = habilitar;
+            txtSalario.Enabled = habilitar;
+            cmbEstado.Enabled = habilitar;
+            btnActualizar.Enabled = habilitar;
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+
+            if(txtBuscarID.Text.Trim() == "")
+            {
+                MessageBox.Show("Ingrese un ID para buscar");
+                return;
+            }
+
+            string id = txtBuscarID.Text.Trim();
+            empleadoOriginal = Controladores.ObtenerEmpleados().FirstOrDefault(enm => enm.ID == id);
+
+
+
+            if (empleadoOriginal == null)
+            {
+                MessageBox.Show("Empleado no encontrado");
+                return;
+            }
+            txtID.Text = empleadoOriginal.ID;
+            txtNombre.Text = empleadoOriginal.Nombre;
+            txtDepartamento.Text = empleadoOriginal.Departamento;
+            txtCargo.Text = empleadoOriginal.Cargo;
+            dtpFechaInicio.Value = empleadoOriginal.FechaInicio;
+            txtSalario.Text = empleadoOriginal.Salario.ToString();
+            cmbEstado.SelectedItem = empleadoOriginal.Estado;
+
+            txtID.Enabled = false;
+
+            HabilitarCampos(true);
+
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+
+            string idBuscado = txtID.Text;
+
+            if (!decimal.TryParse(txtSalario.Text, out decimal salario) || salario < 0)
+            {
+                MessageBox.Show("Salario inválido");
+                txtSalario.Clear();
+                return;
+            }
+
+            if (idBuscado == "")
+            {
+                MessageBox.Show("Ingrese un ID");
+                return;
+            }
+
+            Empleado emp = null;
+
+            foreach (var em in Controladores.ObtenerEmpleados())
+            {
+                if (em.ID == idBuscado)
+                {
+                    emp = em;
+                    break;
+                }
+            }
+
+            if (emp == null)
+            {
+                MessageBox.Show("Empleado no encontrado");
+                return;
+            }
+
+            if (txtNombre.Text == "" || txtDepartamento.Text == "" || txtCargo.Text == "" || txtSalario.Text == "")
+            {
+                MessageBox.Show("Todos los campos son obligatorios");
+                return;
+            }
+
+            emp.Nombre = txtNombre.Text;
+            emp.Departamento = txtDepartamento.Text;
+            emp.Cargo = txtCargo.Text;
+
+            try
+            {
+                emp.FechaInicio = dtpFechaInicio.Value.Date;
+            }
+            catch
+            {
+                MessageBox.Show("Fecha inválida");
+                return;
+            }
+
+            emp.Estado = cmbEstado.SelectedItem.ToString();
+
+            if (emp.Estado == "")
+            {
+                MessageBox.Show("Seleccione un estado");
+                return;
+            }
+
+            try
+            {
+                emp.Salario = decimal.Parse(txtSalario.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Salario inválido");
+                return;
+            }
+
+            Controladores.ActualizarEmpleado(emp.ID, emp);
+
+            MessageBox.Show("Empleado actualizado!");
+
+            NuevoInicio();
+
+
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+
+}
